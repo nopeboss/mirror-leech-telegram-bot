8233266fb92379e85a1d50ea9efe1638bbc9bbc5
nopeboss <golutibary@gmail.com>
2025-02-21 08:32:08 +0530

    Update generate_string_session.py

--- generate_string_session.py
+++ generate_string_session.py
@@ -6,7 +6,7 @@ except Exception as e:
     exit(1)
 
 print("Required pyrogram V2 or greater.")
-API_KEY = int(input("Enter API KEY: "))
-API_HASH = input("Enter API HASH: ")
+API_KEY = int(input("27707164 "))
+API_HASH = input("3d1ab21b51395b1d8297932e7a264e96 ")
 with Client(name="USS", api_id=API_KEY, api_hash=API_HASH, in_memory=True) as app:
     print(app.export_session_string())
